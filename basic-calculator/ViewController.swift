a34065ada1c7e81957b9f9bbc1190515c1a36e80
zyx-0314 <ramirezian037@gmail.com>
2024-08-19 15:39:29 +0800

    feat: added code

--- basic-calculator/ViewController.swift
+++ basic-calculator/ViewController.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var number1: UITextField!
+    @IBOutlet weak var number2: UITextField!
+    @IBOutlet weak var result: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnAdd(_ sender: Any) {
+        let num1 = Double(number1.text!)!
+        let num2 = Double(number2.text!)!
+            
+        result.text = String(num1 + num2)
+    }
+    
+    @IBAction func btnSub(_ sender: Any) {
+        let num1 = Double(number1.text!)!
+        let num2 = Double(number2.text!)!
+        
+        result.text = String(num1 - num2)
+        
+    }
+    
+    @IBAction func btnDiv(_ sender: Any) {
+        let num1 = Double(number1.text!)!
+        let num2 = Double(number2.text!)!
+        
+        result.text = String(num1 / num2)
+    }
+    
+    @IBAction func btnMul(_ sender: Any) {
+        let num1 = Double(number1.text!)!
+        let num2 = Double(number2.text!)!
+        
+        result.text = String(num1 * num2)
+    }
+    
+    @IBAction func blur(_ sender: Any) {
+        number1.resignFirstResponder()
+        number2.resignFirstResponder()
+    }
 }
 
